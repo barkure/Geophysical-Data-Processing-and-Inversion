fa60ddf3a7657db77b5bde3cb2411cc1ba0d95c6
barkure <43804451+barkure@users.noreply.github.com>
2024-04-15 18:17:44 +0800

    Add SOR iteration method implementation and test

--- 实验一/超松弛迭代法/test.py
+++ 实验一/超松弛迭代法/test.py
@@ -1,3 +1,13 @@
 # Description: SOR迭代法测试
 from SOR import SOR
 
+A = [[0.68, 0.01, 0.12],
+     [0.03, -0.54, -0.05],
+     [0.2, 0.08, 0.74]]
+b = [1, 1, 1]
+x0 = [0, 0, 0]
+w = 1.07  # 松弛因子w为1.07
+
+x, n = SOR(A, b, x0, w)
+print("Solution: ", x)
+print("Number of iterations: ", n)
